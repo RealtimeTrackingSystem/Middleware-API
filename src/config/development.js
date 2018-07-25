636d4ce83b9617f54197881e23d34cfa3dc1cd42
John Higgins M. Avila <johnhiggins.avila@gmail.com>
2018-07-26 01:22:04 +0800

    Added Sendgrid env vars

--- src/config/development.js
+++ src/config/development.js
@@ -5,7 +5,12 @@ const CONFIG = {
   },
   SALT: 10,
   SECRET_KEY: 'thesupersecretkey',
-  SESSION_SECRET: 'superscretsession'
+  SESSION_SECRET: 'superscretsession',
+  SENDGRID: {
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    SENDGRID_PASSWORD: process.env.SENDGRID_PASSWORD,
+    SENDGRID_USERNAME: process.env.SENDGRID_USERNAME
+  }
 };
 
 module.exports = CONFIG;
--- src/config/production.js
+++ src/config/production.js
@@ -5,7 +5,12 @@ const CONFIG = {
   },
   SALT: process.env.SALT || 10,
   SECRET_KEY: process.env.SECRET_KEY || 'thesupersecretkey',
-  SESSION_SECRET: process.env.SESSION_SECRET || 'superscretsession'
+  SESSION_SECRET: process.env.SESSION_SECRET || 'superscretsession',
+  SENDGRID: {
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    SENDGRID_PASSWORD: process.env.SENDGRID_PASSWORD,
+    SENDGRID_USERNAME: process.env.SENDGRID_USERNAME
+  }
 };
 
 module.exports = CONFIG;
