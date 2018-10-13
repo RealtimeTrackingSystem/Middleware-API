f6e6b31b0ce89a42f608b48fad134ad56fd2837b
John Higgins Avila <johnhiggins.avila@gmail.com>
2018-10-13 07:50:37 -0700

    id: ISSUES-64 G-Cloud Deployment (#16)

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 docker build -t johnhigginsavila/middleware-api:latest -t johnhigginsavila/middleware-api:$SHA -f ./Dockerfile .
 docker push johnhigginsavila/middleware-api:latest
 docker push johnhigginsavila/middleware-api:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/middleware-api-deployment middleware-api=johnhigginsavila/middleware-api:$SHA
