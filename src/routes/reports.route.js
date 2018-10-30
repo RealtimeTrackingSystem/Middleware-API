29f895fe776ff821f5ca1caf5dbf283321d2c837
John Higgins M. Avila <johnhiggins.avila@gmail.com>
2018-10-30 15:48:18 +0800

    updating report

--- src/routes/reports.route.js
+++ src/routes/reports.route.js
@@ -11,10 +11,14 @@ reportRoute.get('/api/reports',
   handlers.reports.getReports.logic);
 
 reportRoute.post('/api/reports',
+  handlers.media.uploads.multipleUpload('reports', 4, 'reports'),
   handlers.auth.authentication.requireAuth,
   handlers.auth.authentication.authenticate,
   handlers.auth.authentication.logActivity,
   handlers.reports.postReport.validateBody,
+  handlers.reports.postReport.processMediaUploads,
+  handlers.reports.postReport.processTags,
+  handlers.reports.postReport.processPeopleAndProperties,
   handlers.reports.postReport.sendReport,
   handlers.reports.postReport.respond);
 
