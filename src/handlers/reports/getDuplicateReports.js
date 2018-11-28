211f0e51f2ba4677fa5a027123416f32aadeb64c
John Higgins M. Avila <johnhiggins.avila@gmail.com>
2018-11-28 12:11:11 +0800

    added get duplicate reports

---
+++ src/handlers/reports/getDuplicateReports.js
@@ -0,0 +1,36 @@
+function logic (req, res, next) {
+  const isDuplicate = req.query.isDuplicate;
+  return req.api.report.getDuplicates(isDuplicate)
+    .then(response => {
+      req.$scope.reports = response.reports;
+      next();
+    })
+    .catch(function (err) {
+      if (err.httpCode) {
+        req.logger.warn(err, 'GET /api/reports/duplicates');
+        return res.status(err.httpCode).send(err);
+      }
+      req.logger.error(err, 'GET /api/reports/duplicates');
+      res.status(500).send({
+        status: 'ERROR',
+        statusCode: 1,
+        httpCode: 500,
+        message: 'Internal Server Error'
+      });
+    });
+}
+
+function respond (req, res) {
+  req.logger.info(req.$scope.report, 'GET /api/reports/duplicates');
+  res.status(200).send({
+    status: 'SUCCESS',
+    statusCode: 0,
+    httpCode: 200,
+    reports: req.$scope.reports
+  });
+}
+
+module.exports = {
+  logic,
+  respond
+};
