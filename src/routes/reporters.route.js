2d23fdfd84a0ae19c3be65c5d114ec3c2482305b
John Higgins Avila <johnhiggins.avila@gmail.com>
2018-12-05 14:21:50 +0800

    Updated Reporter Schema (#54)

--- src/routes/reporters.route.js
+++ src/routes/reporters.route.js
@@ -3,6 +3,13 @@ const handlers = require('../handlers');
 
 const reporterRoute = express.Router();
 
+reporterRoute.post('/api/reporters/firebase',
+  handlers.auth.authentication.requireAuth,
+  handlers.auth.authentication.authenticate,
+  handlers.auth.authentication.logActivity,
+  handlers.reporters.addOrUpdateFirebaseToken.validateBody,
+  handlers.reporters.addOrUpdateFirebaseToken.logic);
+
 reporterRoute.get('/api/reporters/:reporterId',
   handlers.auth.authentication.requireAuth,
   handlers.auth.authentication.authenticate,
